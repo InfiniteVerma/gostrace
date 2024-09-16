b41462e55ba0374e5cbb3d5e834c8dc5e70d38db
InfiniteVerma <vermaanant25@gmail.com>
2024-09-16 20:45:48 +0530

    basic cli

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
     "fmt"
+    "os"
 )
 
 func main() {
-    fmt.Println("Hello world")
+    
+    args_length := len(os.Args)
+
+    if args_length != 2 {
+        fmt.Println("ERROR invalid number of args passed. Usage: ./goptrace <pid>")
+        os.Exit(0)
+    }
+
+    pid := os.Args[1]
+
+    fmt.Println("Starting ptrace on pid:", pid)
 }
